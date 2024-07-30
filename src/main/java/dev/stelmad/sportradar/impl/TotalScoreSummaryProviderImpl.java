2c59c57b160820fe95ffa870f4f4eb0bff4322bc
Daniil Stelmakh <dan.stelmakh@gmail.com>
2024-07-30 16:06:48 +0200

    Add jdocs for getSummary()

--- src/main/java/dev/stelmad/sportradar/impl/TotalScoreSummaryProviderImpl.java
+++ src/main/java/dev/stelmad/sportradar/impl/TotalScoreSummaryProviderImpl.java
@@ -9,14 +9,30 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Implementation of the SummaryProvider interface that provides a summary of the scoreboard
+ * sorted by total score.
+ */
 public class TotalScoreSummaryProviderImpl implements SummaryProvider {
 
     private final Scoreboard scoreboard;
 
+    /**
+     * Constructs a TotalScoreSummaryProviderImpl with the specified scoreboard.
+     *
+     * @param scoreboard the scoreboard to be used for generating the summary
+     * @throws NullPointerException if the scoreboard is null
+     */
     public TotalScoreSummaryProviderImpl(Scoreboard scoreboard) {
         this.scoreboard = Objects.requireNonNull(scoreboard);
     }
 
+    /**
+     * Returns a summary of the scoreboard records sorted by total score in descending order.
+     * If two records have the same total score, they are sorted by their original order.
+     *
+     * @return a list of scoreboard records sorted by total score in descending order
+     */
     @Override
     public List<ScoreboardRecord> getSummary() {
         List<ScoreboardRecord> summary = new ArrayList<>(this.scoreboard.getRecordList());
